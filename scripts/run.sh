72fff02775c5991f0e883f9bb2ab2e96a9634d36
busyDennis <denisbezverbnyy@gmail.com>
2017-06-30 19:59:59 -0400

    Added a start script in the parent folder

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-export NODE_ENV=development
+
+#Kill all child processes on exit with 'trap'
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
 firejail firefox  &
+
+#Display Angular documentation:
 #node ~/.npm-global/bin/http-server front-end/docs -a 127.0.0.1 -p 8081 &
 http-server front-end/docs -a 127.0.0.1 -p 8081 &
+
+#Start the web application
 cd front-end
 npm start &
 cd ../back-end
