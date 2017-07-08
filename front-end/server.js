507858cb08fb081588d3af7f45c833f73f4f0b51
busyDennis <denisbezverbnyy@gmail.com>
2017-07-07 20:40:27 -0400

    More changes to facilitate Heroku deployment

--- front-end/server.js
+++ front-end/server.js
@@ -1,8 +1,10 @@
 var connect = require('connect');
 var opn = require('opn');
 
-connect().use(serveStatic(__dirname, {'index': ['index.html']})).listen(8080, function() {
+connect().listen(8080, function() {
   console.log('Server running on 8080...');
 });
+
+//use(serveStatic(__dirname, {'index': ['index.html']})).
 
 opn('https://hidden-plateau-18437.herokuapp.com:8080', { app: process.argv[2] });
